9b6c8c81d63ea1c222d9ee34a45d5e69b21430c2
Evdokimov Denis <analitik9@gmail.com>
2024-06-18 16:30:51 +0300

    Initial Commit

---
+++ habit-tracking/habit_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackingApp.swift
+//  habit-tracking
+//
+//  Created by Denis Evdokimov on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
